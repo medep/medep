bfc119020e9a7df2f143c4d6b0ebdf04a483bae7
Janez Kranjc <janez.kranjc@ijs.si>
2020-04-22 16:20:12 +0200

    Remove hello world reference

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -40,14 +40,9 @@
 </template>
 
 <script>
-    import HelloWorld from './components/HelloWorld';
 
     export default {
         name: 'App',
-
-        components: {
-            HelloWorld,
-        },
 
         data: () => ({
             drawer: null,
