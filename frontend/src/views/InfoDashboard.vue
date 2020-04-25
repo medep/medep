1ab3e63de12b315704bc9a494bd1e6f53f886974
Janez Kranjc <janez.kranjc@ijs.si>
2020-04-26 00:26:28 +0200

    Tabs

--- frontend/src/views/InfoDashboard.vue
+++ frontend/src/views/InfoDashboard.vue
@@ -6,10 +6,70 @@
         >
             <v-col>
                 <div style="height:300px;">
-                <info-graph></info-graph>
+                    <info-graph></info-graph>
                 </div>
 
 
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <br><br><br><br>
+                <v-tabs
+                        fixed-tabs
+                >
+                    <v-tab>
+                        Medical Protocol
+                    </v-tab>
+                    <v-tab>
+                        News
+                    </v-tab>
+                    <v-tab>
+                        Another Selection
+                    </v-tab>
+                    <v-tab>
+                        Another Tab
+                    </v-tab>
+
+
+                    <v-tab-item>
+                        <v-card
+                                flat
+                                tile
+                        >
+                            <v-card-text>blabla</v-card-text>
+                        </v-card>
+                    </v-tab-item>
+
+                    <v-tab-item>
+                        <v-card
+                                flat
+                                tile
+                        >
+                            <v-card-text>lorem ipsum...</v-card-text>
+                        </v-card>
+                    </v-tab-item>
+
+                    <v-tab-item>
+                        <v-card
+                                flat
+                                tile
+                        >
+                            <v-card-text>neki tretjega.</v-card-text>
+                        </v-card>
+                    </v-tab-item>
+
+                    <v-tab-item>
+                        <v-card
+                                flat
+                                tile
+                        >
+                            <v-card-text>arlarl</v-card-text>
+                        </v-card>
+                    </v-tab-item>
+
+
+                </v-tabs>
             </v-col>
         </v-row>
     </v-container>
