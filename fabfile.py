71315e40c2e1b9fa220433375cdb333b3e1153da
Janez Kranjc <janez.kranjc@ijs.si>
2020-04-26 19:13:07 +0200

    Add fabfile

---
+++ fabfile.py
@@ -0,0 +1,23 @@
+from __future__ import with_statement
+from fabric.api import *
+from fabric.colors import *
+from fabric.utils import puts
+
+base_path = os.path.abspath(os.path.dirname(__file__))
+
+env.forward_agent = True
+env.user = 'ubuntu'
+env.key_filename = os.path.join(base_path, 'keys/medep_rsa')
+env.host_string = 'medep.org:22'
+
+@task
+def deploy():
+    with cd('/home/ubuntu/mpg/'):
+        puts(magenta("[Pulling master]"))
+        run('git pull origin master')
+
+        puts(magenta("[Updating containers]"))
+        run('docker-compose up -d --build')
+
+
+
