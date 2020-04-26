8de312558a81f1b962e7b0625803aa697d3f7007
Janez Kranjc <janez.kranjc@ijs.si>
2020-04-27 01:21:54 +0200

    Add protocols text

--- frontend/src/views/InfoDashboard.vue
+++ frontend/src/views/InfoDashboard.vue
@@ -76,7 +76,192 @@
                                     flat
                                     tile
                             >
-                                <v-card-text>blabla</v-card-text>
+                                <v-card-text>
+
+
+                                    <p>Covid-19 therapy (potential therapeutic options for treating COVID-19):</p>
+                                    <p>Direct effect on SARS-CoV-2:</p>
+                                    <ul class="mb-3">
+                                        <li>Favipiravir</li>
+                                        <li>Chloroquine and hydroxychloroquine</li>
+                                        <li>Azytromicin</li>
+                                        <li>Remdesivir</li>
+                                        <li>Lopinavir/ritonavir (Kaletra)</li>
+                                        <li>Umifenovir</li>
+                                    </ul>
+                                    <p>Indirect effect on SARS-CoV-2:</p>
+                                    <ul class="mb-3">
+                                        <li>Tocilizumab</li>
+                                        <li>Losartan</li>
+                                    </ul>
+                                    <p>Appropriate supportive care needed (Oxygen, anticoagulant treatment,
+                                        &hellip;)</p>
+                                    <p>&nbsp;</p>
+                                    <p>Protocols:</p>
+                                    <p>In relation to the phase of the epidemy</p>
+                                    <p>Phase 0:</p>
+                                    <ul>
+                                        <li>Intensive testing of potential suspected cases, doing overtesting to try
+                                            finding infected individuals. Education of the public about signs and
+                                            symptoms of COVID-19.
+                                        </li>
+                                        <li>Procurement procedures from the government for acquiring personal protective
+                                            equipment (PPE) for medical health workers
+                                        </li>
+                                        <li>Designating hospitals to treat the first COVID 19 patients</li>
+                                        <li>Education of health workers on how to treat such patients, how to put on/off
+                                            protective gear
+                                        </li>
+                                        <li>Setting up several testing points to test for COVID-19</li>
+                                    </ul>
+                                    <p>&nbsp;</p>
+                                    <ul>
+                                        <li><em>If a patient is ill and shows respiratory symptoms, they should call
+                                            their doctor and check with them wheater to get tested. They should avoid
+                                            any unnecessary contact and stay at home, and not go to their doctor
+                                            personally.</em></li>
+                                        <li><em>In a medical emergency calling the emergency medical help number telling
+                                            the personel that they also have respiratory symptoms, so they can be
+                                            properly protected. Testing the patient.</em></li>
+                                    </ul>
+                                    <br>
+                                    <p>Phase 1:</p>
+                                    <ul>
+                                        <li>First positive patients, alltogether less then n patients. Intensifying
+                                            epidemiologic efforts to quell the transmision of the disease, finding close
+                                            contact of the patients, isolating/hospitalising the patients and
+                                            quarantining close contacts with all the needed information.
+                                        </li>
+                                        <li>Intensifying PPE procurement procdures.</li>
+                                        <li>Epidemiologists coordinating with the government to prepare protocols for
+                                            possible outcomes. Preparing the legislature needed for activating
+                                            additional personel, managing the epidemiologic crisis.
+                                        </li>
+                                        <li>Intensifying the public awareness of the disease transmision and preventive
+                                            measures.
+                                        </li>
+                                        <li>Advising against public gatherings indoors, canceling events of over 500
+                                            people indoors. Advising the elderly to stay secluded.
+                                        </li>
+                                        <li>Setting up additional testing locations as needed.</li>
+                                        <li>Setting up a call center for the general public in cooperation with the
+                                            medical students
+                                        </li>
+                                    </ul>
+                                    <p>&nbsp;</p>
+                                    <ul>
+                                        <li><em>Setting up protocols for COVID-19 treatment options.</em></li>
+                                        <li><em>Setting up the 24/7 call numbers for other doctors to reach the
+                                            infectology department, micribiology laboratory and on call
+                                            epidemiologists.</em></li>
+                                        <li><em>Setting up designated wards with an appropriate isolation option fort he
+                                            COVID+19 positive patients.</em></li>
+                                        <li><em>Setting up protocols fort he transportation of those patients in the
+                                            hospital, and to the needed diagnostic procedures.</em></li>
+                                        <li><em>Getting checked by a doctor in ful PPE, if stable &ndash; they can go
+                                            home and wait for the test results. They get additional written information
+                                            about the possible disease progression and what they need to look out
+                                            for.</em></li>
+                                        <li><em>If unstable &ndash; hospitalisation required with proper isolation
+                                            protocols (single isolation or cohort isolation). Waiting fort the test
+                                            results in the hospital.</em></li>
+                                        <li><em>If the patient proves to be positive for COVID-19, according to the
+                                            protocols.</em></li>
+                                        <li><em>Hospitalizing the first patients in order fort the healthcare workers to
+                                            get the needed experience handling these patients, getting to know the
+                                            disease progression.</em></li>
+                                        <li><em>Minimizing the number of entries to the essential healthcare
+                                            infrastructure, doing triage at the entrypoint.</em></li>
+                                    </ul>
+                                    <br>
+                                    <p>Phase 2:</p>
+                                    <ul>
+                                        <li>After a set number of positive patients and proven community transmision
+                                            (without a known contact with a positive patient or travel abroad), the
+                                            government institutes the before prepared protocols, declaring a coronavirus
+                                            epidemic.
+                                        </li>
+                                        <li>No longer monitoring the specific contacts of positive patients, since there
+                                            already is significant community transmission (except for the high risk
+                                            groups &ndash; elderly homes, &hellip;).
+                                        </li>
+                                        <li>Closing schools, kindergartens, bars, restaurants, advising to work from
+                                            home. Temporarilly closing down non-essential buisinesses, only leaving open
+                                            the essential infrastructure.
+                                        </li>
+                                        <li>Legislature provisions prepared beforehand are implemented. Essential
+                                            workers are prohibited from leaving the country, no vacations allowed. They
+                                            can be called upon to work in different regions according to the healthcare
+                                            needs of different regions in the country.
+                                        </li>
+                                        <li>Stoping public transport.</li>
+                                        <li>Obligatory masks/mouth and nose coverings in public spaces, shops,...</li>
+                                        <li>Setting up specific timeslots fort he most vulnerable population to be able
+                                            to use the non healthcare related essential infrastructure.
+                                        </li>
+                                    </ul>
+                                    <p>&nbsp;</p>
+                                    <ul>
+                                        <li><em>Stopping all non-essential (life-threatening) procedures and diagnostics
+                                            in hospitals.</em></li>
+                                        <li><em>Further education of the workforce about the disease in coordination
+                                            with international experiences (Webinars, lectures, &hellip;)</em></li>
+                                        <li><em>Cooperation of different specialists to treat the patient and the
+                                            disease complications, organizing smaller task-oriented teams to further
+                                            study treatment options,&hellip;)</em></li>
+                                        <li><em>Having a low treshold to test healthcare workers for COVID 19 in case of
+                                            potential symptoms.</em></li>
+                                        <li><em>Preparing for a possible overflow mof patients, setting up
+                                            protocols.</em></li>
+                                        <li><em>Setting up telemetric medicine for the risk group patients &ndash; over
+                                            60 years of age, immunocompromised, pregnant patients, monitoring them
+                                            closely at home.</em></li>
+                                    </ul>
+                                    <br>
+                                    <p>Phase 3:</p>
+                                    <ul>
+                                        <li>Observing the effects of the implemented restrictions in effect with the
+                                            disease transmission paterns.
+                                        </li>
+                                        <li>Preparing additional restriction if needed.</li>
+                                        <li>If reaching a plateau, possibly loosening the restrictions on public life
+                                            and movement.
+                                        </li>
+                                    </ul>
+                                    <br>
+                                    <p>Phase 4:</p>
+                                    <ul>
+                                        <li>Gradually loosening the restrictions on public life, reopening businesses.
+                                        </li>
+                                        <li>Again intensive epidemiologic following of the contacts of the newly found
+                                            positive patients.
+                                        </li>
+                                    </ul>
+                                    <p>&nbsp;</p>
+                                    <ul>
+                                        <li><em>Consider reopening the non-essential procedures and diagnostic
+                                            procedures.</em></li>
+                                    </ul>
+                                    <br>
+                                    <p>Phase 5:</p>
+                                    <ul>
+                                        <li>The resolution of the epidemy, reopening the healthcare to the non-essential
+                                            procedures and diagnostics
+                                        </li>
+                                    </ul>
+                                    <br>
+                                    <p>Phase 6:</p>
+                                    <ul>
+                                        <li>In case of a worsening epidemic, implementing the national catastrophy plan,
+                                            activating the millitary and reserve workforce, increasing the bed capacity
+                                            by setting up field hospitals, calling retired doctors back to work, asking
+                                            for international help economically, for supplies or needed workforce.
+                                        </li>
+                                    </ul>
+                                    <p>&nbsp;</p>
+
+
+                                </v-card-text>
                             </v-card>
                         </v-tab-item>
 
