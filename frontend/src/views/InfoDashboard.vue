81f5a78dfbbc0a6d1c6091e094a55b673c447413
Martin Breskvar <martin.breskvar@ijs.si>
2020-04-26 21:18:53 +0200

    InfoDashboard.vue legislation updates

--- frontend/src/views/InfoDashboard.vue
+++ frontend/src/views/InfoDashboard.vue
@@ -57,14 +57,8 @@
 								1.
 							</td>
 							<td>
-								834.
-							</td>
-							<td>
 								<a target="_blank" href="https://www.uradni-list.si/glasilo-uradni-list-rs/vsebina/2020-01-0834/sklep-o-izmenjavi-osebnih-podatkov-o-odrejenih-posebnih-ukrepih-osamitve-karantene-in-obveznega-zdravljenja-zaradi-bolezni-covid-19-med-nacionalnim-institutom-za-javno-zdravje-in-policijo?h=covid">Sklep o izmenjavi osebnih podatkov o odrejenih posebnih ukrepih osamitve, karantene in obveznega zdravljenja zaradi bolezni COVID-19 med Nacionalnim inštitutom za javno zdravje in Policijo</a>
-								<div class="results-note">
-									UL št.
-									<span class="bold">56</span>, 22. 4. 2020 / Sklepi
-								</div>
+								<div class="results-note">22. 4. 2020</div>
 							</td>
 						</tr>
 						
@@ -73,14 +67,8 @@
 								2.
 							</td>
 							<td>
-								775.
-							</td>
-							<td>
 								<a target="_blank" href="https://www.uradni-list.si/glasilo-uradni-list-rs/vsebina/2020-01-0775/odlok-o-odrejanju-in-izvajanju-ukrepov-povezanih-s-preprecevanjem-sirjenja-covid-19-na-mejnih-prehodih-na-zunanji-meji-in-na-kontrolnih-tockah-na-notranjih-mejah-republike-slovenije?h=covid">Odlok o odrejanju in izvajanju ukrepov, povezanih s preprečevanjem širjenja COVID-19, na mejnih prehodih na zunanji meji in na kontrolnih točkah na notranjih mejah Republike Slovenije</a>
-								<div class="results-note">
-									UL št.
-									<span class="bold">50</span>, 11. 4. 2020 / Odloki
-								</div>
+								<div class="results-note">11. 4. 2020</div>
 							</td>
 						</tr>
 						
@@ -89,14 +77,8 @@
 								3.
 							</td>
 							<td>
-								773.
-							</td>
-							<td>
 								<a target="_blank" href="https://www.uradni-list.si/glasilo-uradni-list-rs/vsebina/2020-01-0773/odlok-o-dopolnitvi-odloka-o-zacasnih-ukrepih-na-podrocju-zdravstvene-dejavnosti-zaradi-zajezitve-in-obvladovanja-epidemije-covid-19?h=covid">Odlok o dopolnitvi Odloka o začasnih ukrepih na področju zdravstvene dejavnosti zaradi zajezitve in obvladovanja epidemije COVID-19</a>
-								<div class="results-note">
-									UL št.
-									<span class="bold">49</span>, 10. 4. 2020 / Odloki
-								</div>
+								<div class="results-note">10. 4. 2020</div>
 							</td>
 						</tr>
 						
@@ -105,14 +87,8 @@
 								4.
 							</td>
 							<td>
-								766.
-							</td>
-							<td>
 								<a target="_blank" href="https://www.uradni-list.si/glasilo-uradni-list-rs/vsebina/2020-01-0766/zakon-o-interventnih-ukrepih-za-zajezitev-epidemije-covid-19-in-omilitev-njenih-posledic-za-drzavljane-in-gospodarstvo-ziuzeop?h=covid">Zakon o interventnih ukrepih za zajezitev epidemije COVID-19 in omilitev njenih posledic za državljane in gospodarstvo (ZIUZEOP)</a>
-								<div class="results-note">
-									UL št.
-									<span class="bold">49</span>, 10. 4. 2020 / Zakoni, obvezne razlage zakonov
-								</div>
+								<div class="results-note">10. 4. 2020</div>
 							</td>
 						</tr>
 						
@@ -121,14 +97,8 @@
 								5.
 							</td>
 							<td>
-								708.
-							</td>
-							<td>
 								<a target="_blank" href="https://www.uradni-list.si/glasilo-uradni-list-rs/vsebina/2020-01-0708/odlok-o-zacasnih-ukrepih-na-podrocju-zdravstvene-dejavnosti-zaradi-zajezitve-in-obvladovanja-epidemije-covid-19?h=covid">Odlok o začasnih ukrepih na področju zdravstvene dejavnosti zaradi zajezitve in obvladovanja epidemije COVID-19</a>
-								<div class="results-note">
-									UL št.
-									<span class="bold">40</span>, 31. 3. 2020 / Odloki
-								</div>
+								<div class="results-note">31. 3. 2020</div>
 							</td>
 						</tr>
 						
@@ -137,14 +107,8 @@
 								6.
 							</td>
 							<td>
-								700.
-							</td>
-							<td>
 								<a target="_blank" href="https://www.uradni-list.si/glasilo-uradni-list-rs/vsebina/2020-01-0700/sklep-o-delu-sodnega-sveta-rs-v-casu-trajanja-odredbe-o-razglasitvi-epidemije-nalezljive-bolezni-sars-cov-2-covid-19?h=covid">Sklep o delu Sodnega sveta RS v času trajanja Odredbe o razglasitvi epidemije nalezljive bolezni SARS-CoV-2 (COVID-19)</a>
-								<div class="results-note">
-									UL št.
-									<span class="bold">39</span>, 30. 3. 2020 / SODNI SVET
-								</div>
+								<div class="results-note">30. 3. 2020</div>
 							</td>
 						</tr>
 						
@@ -153,14 +117,8 @@
 								7.
 							</td>
 							<td>
-								681.
-							</td>
-							<td>
 								<a target="_blank" href="https://www.uradni-list.si/glasilo-uradni-list-rs/vsebina/2020-01-0681/zakon-o-zacasnih-ukrepih-v-zvezi-s-sodnimi-upravnimi-in-drugimi-javnopravnimi-zadevami-za-obvladovanje-sirjenja-nalezljive-bolezni-sars-cov-2-covid-19-zzusudjz?h=covid">Zakon o začasnih ukrepih v zvezi s sodnimi, upravnimi in drugimi javnopravnimi zadevami za obvladovanje širjenja nalezljive bolezni SARS-CoV-2 (COVID-19) (ZZUSUDJZ)</a>
-								<div class="results-note">
-									UL št.
-									<span class="bold">36</span>, 28. 3. 2020 / Zakoni, obvezne razlage zakonov
-								</div>
+								<div class="results-note">28. 3. 2020</div>
 							</td>
 						</tr>
 						
@@ -169,14 +127,8 @@
 								8.
 							</td>
 							<td>
-								657.
-							</td>
-							<td>
 								<a target="_blank" href="https://www.uradni-list.si/glasilo-uradni-list-rs/vsebina/2020-01-0657/odlok-o-zacasnih-ukrepih-pri-izvajanju-obveznega-zdravstvenega-zavarovanja-zaradi-zajezitve-in-obvladovanja-epidemije-covid-19?h=covid">Odlok o začasnih ukrepih pri izvajanju obveznega zdravstvenega zavarovanja zaradi zajezitve in obvladovanja epidemije COVID-19</a>
-								<div class="results-note">
-									UL št.
-									<span class="bold">34</span>, 24. 3. 2020 / Odloki
-								</div>
+								<div class="results-note">24. 3. 2020</div>
 							</td>
 						</tr>
 						
@@ -185,14 +137,8 @@
 								9.
 							</td>
 							<td>
-								650.
-							</td>
-							<td>
 								<a target="_blank" href="https://www.uradni-list.si/glasilo-uradni-list-rs/vsebina/2020-01-0650/odredba-o-zacasnem-delovnem-casu-notarjev-v-casu-trajanja-epidemije-virusne-okuzbe-sars-cov-2-covid-19?h=covid">Odredba o začasnem delovnem času notarjev v času trajanja epidemije virusne okužbe SARS-CoV-2 (COVID-19)</a>
-								<div class="results-note">
-									UL št.
-									<span class="bold">33</span>, 23. 3. 2020 / MINISTRSTVA
-								</div>
+								<div class="results-note">23. 3. 2020</div>
 							</td>
 						</tr>
 						
@@ -201,14 +147,8 @@
 								10.
 							</td>
 							<td>
-								645.
-							</td>
-							<td>
 								<a target="_blank" href="https://www.uradni-list.si/glasilo-uradni-list-rs/vsebina/2020-01-0645/odlok-o-zacasnih-ukrepih-na-podrocju-zdravstvene-dejavnosti-zaradi-zajezitve-in-obvladovanja-epidemije-covid-19?h=covid">Odlok o začasnih ukrepih na področju zdravstvene dejavnosti zaradi zajezitve in obvladovanja epidemije COVID-19</a>
-								<div class="results-note">
-									UL št.
-									<span class="bold">32</span>, 20. 3. 2020 / Odloki
-								</div>
+								<div class="results-note">20. 3. 2020</div>
 							</td>
 						</tr>
 						
@@ -217,14 +157,8 @@
 								11.
 							</td>
 							<td>
-								558.
-							</td>
-							<td>
 								<a target="_blank" href="https://www.uradni-list.si/glasilo-uradni-list-rs/vsebina/2020-01-0558/sklep-o-uporabi-ukrepov-ki-jih-doloca-zakon-o-nalezljivih-boleznih-pri-epidemiji-covid-19?h=covid">Sklep o uporabi ukrepov, ki jih določa Zakon o nalezljivih boleznih, pri epidemiji COVID-19</a>
-								<div class="results-note">
-									UL št.
-									<span class="bold">23</span>, 14. 3. 2020 / Sklepi
-								</div>
+								<div class="results-note">14. 3. 2020</div>
 							</td>
 						</tr>
 						
@@ -233,14 +167,8 @@
 								12.
 							</td>
 							<td>
-								550.
-							</td>
-							<td>
 								<a target="_blank" href="https://www.uradni-list.si/glasilo-uradni-list-rs/vsebina/2020-01-0550/odredba-o-zacasnih-ukrepih-za-obvladovanje-sirjenja-nalezljive-bolezni-sars-cov-2-covid-19?h=covid">Odredba o začasnih ukrepih za obvladovanje širjenja nalezljive bolezni SARS-CoV-2 (COVID-19)</a>
-								<div class="results-note">
-									UL št.
-									<span class="bold">22</span>, 13. 3. 2020 / MINISTRSTVA
-								</div>
+								<div class="results-note">13. 3. 2020</div>
 							</td>
 						</tr>
 						
@@ -249,14 +177,8 @@
 								13.
 							</td>
 							<td>
-								532.
-							</td>
-							<td>
 								<a target="_blank" href="https://www.uradni-list.si/glasilo-uradni-list-rs/vsebina/2020-01-0532/odredba-o-razglasitvi-epidemije-nalezljive-bolezni-sars-cov-2-covid-19-na-obmocju-republike-slovenije?h=covid">Odredba o razglasitvi epidemije nalezljive bolezni SARS-CoV-2 (COVID-19) na območju Republike Slovenije</a>
-								<div class="results-note">
-									UL št.
-									<span class="bold">19</span>, 12. 3. 2020 / MINISTRSTVA
-								</div>
+								<div class="results-note">12. 3. 2020</div>
 							</td>
 						</tr>
 						
@@ -265,14 +187,8 @@
 								14.
 							</td>
 							<td>
-								525.
-							</td>
-							<td>
 								<a target="_blank" href="https://www.uradni-list.si/glasilo-uradni-list-rs/vsebina/2020-01-0525/odredba-o-zacasnih-ukrepih-za-obvladovanje-sirjenja-nalezljive-bolezni-sars-cov-2-covid-19?h=covid">Odredba o začasnih ukrepih za obvladovanje širjenja nalezljive bolezni SARS-CoV-2 (COVID-19)</a>
-								<div class="results-note">
-									UL št.
-									<span class="bold">18</span>, 11. 3. 2020 / MINISTRSTVA
-								</div>
+								<div class="results-note">11. 3. 2020</div>
 							</td>
 						</tr>
 						
