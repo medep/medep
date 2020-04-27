35585e4355068e817d0b05fd5ebbd9663b04bca4
Martin Breskvar <martin.breskvar@ijs.si>
2020-04-27 09:44:28 +0200

    the team

--- frontend/src/views/Team.vue
+++ frontend/src/views/Team.vue
@@ -10,34 +10,40 @@
                     <v-col class="col-md-4 justify-center text-center">
                         <v-avatar size="200" class="mt-3"><v-img src="../assets/team/martin.jpg"></v-img></v-avatar>
                         <v-card-title class="text-center justify-center">Martin</v-card-title>
-                        <v-card-subtitle>...</v-card-subtitle>
+                        <v-card-subtitle>PhD<br>Jožef Stefan Institute<br>Ljubljana, Slovenia</v-card-subtitle>
                     </v-col>
 
                     <v-col class="col-md-4 justify-center text-center">
                         <v-avatar size="200" class="mt-3"><v-img src="../assets/team/janez.jpg"></v-img></v-avatar>
                         <v-card-title class="text-center justify-center">Janez</v-card-title>
-                        <v-card-subtitle>...</v-card-subtitle>
+                        <v-card-subtitle>PhD<br>Jožef Stefan Institute<br>Ljubljana, Slovenia</v-card-subtitle>
                     </v-col>
 
                     <v-col class="col-md-4 justify-center text-center">
                         <v-avatar size="200" class="mt-3"><v-img src="../assets/team/blaz.png"></v-img></v-avatar>
                         <v-card-title class="text-center justify-center">Blaž</v-card-title>
-                        <v-card-subtitle>...</v-card-subtitle>
+                        <v-card-subtitle>Msc<br>Jožef Stefan Institute<br>Ljubljana, Slovenia</v-card-subtitle>
                     </v-col>
 
                     <v-col class="col-md-4 justify-center text-center">
                         <v-avatar size="200" class="mt-3"><v-img src="../assets/team/matej.png"></v-img></v-avatar>
                         <v-card-title class="text-center justify-center">Matej</v-card-title>
-                        <v-card-subtitle>...</v-card-subtitle>
+                        <v-card-subtitle>MSc<br>Jožef Stefan Institute<br>Ljubljana, Slovenia</v-card-subtitle>
                     </v-col>
 
                     <v-col class="col-md-4 justify-center text-center">
                         <v-avatar size="200" class="mt-3"><v-img src="../assets/team/marko.png"></v-img></v-avatar>
                         <v-card-title class="text-center justify-center">Marko</v-card-title>
-                        <v-card-subtitle>...</v-card-subtitle>
-
+                        <v-card-subtitle>MPharm, PhD<br>University of Maribor, Slovenia</v-card-subtitle>
                     </v-col>
 
+                    <v-col class="col-md-4 justify-center text-center">
+                        <v-avatar size="200" class="mt-3"><v-img src="../assets/team/andrej.jpg"></v-img></v-avatar>
+                        <v-card-title class="text-center justify-center">Andrej</v-card-title>
+                        <v-card-subtitle>MD, Resident of Infectology<br>Department for infectious diseases<br>
+University Clinical Center Ljubljana, Slovenia
+</v-card-subtitle>
+                    </v-col>
                 </v-row>
 
             </v-col>
