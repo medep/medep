bcf740495c59e04ed82887c60bc17ad15aa1a241
Martin Breskvar <martin.breskvar@ijs.si>
2020-04-27 07:02:05 +0200

    solution 3

--- frontend/src/views/Solution.vue
+++ frontend/src/views/Solution.vue
@@ -2,10 +2,7 @@
     <v-container
             fluid
     >
-        <v-row
-                align="center"
-                justify="center"
-        >
+        <v-row                align="center"                justify="center"        >
             <v-col>
 	
 	<h1>Connect all stakeholders and win</h1>
@@ -20,25 +17,66 @@ In an ideal world, each healthcare organization would have in-house software dev
 this is not the case. Healthcare IT ecosystems are incredibly complex, developed and maintained by many vendors and often poorly integrated. 
 This results in technical difficulties and data integrity issues on a daily basis. It is becoming increasingly difficult to obtain quality data. 
 
-</p>
+<br/>
 
-<p>
 MeDEP solves many issues related to data acquisition and comes in two flawors: MeDEP local and MeDEP federated. <br />
-
-MeDEP local is installed within the organization and is integrated with existing information systems by using specially crafted MeDEP connectors.
-
-<v-img
-                            class="white--text align-end"
-                            height="400px"
-                            src="../assets/medep-local"
-                    ></v-img>
 
 </p>
 
             </v-col>
         </v-row>
+		
+		
+		
+		<v-row class="col-10">
+
+            <v-col class="col-md-5">
+
+                <v-card>
+                    <v-img
+                            class="white--text align-end"
+                            height="300px"
+                            src="../assets/medep-local.jpg"
+                    ></v-img>
+                    <v-card-subtitle>MeDEP local: join all organizational data sources
+                    </v-card-subtitle>
+                    <v-card-text>
+                        MeDEP local is installed within the organization and is integrated with existing information systems by using specially crafted MeDEP connectors.
+						The connectors are tailor-made by us to ensure 100% reliability and security.
+                    </v-card-text>
+                </v-card>
+            </v-col>
 				
+   <v-col class="col-md-5">
+
+                <v-card>
+                    <v-img
+                            class="white--text align-end"
+                            height="300px"
+                            src="../assets/medep-federated.jpg"
+                    ></v-img>
+                    <v-card-subtitle>MeDEP federated: Harness the real power of connected world
+                    </v-card-subtitle>
+                    <v-card-text>
+                        MeDEP federated connects multiple MeDEP local instances. With this, different organizations can share research and current business data.
+						All involved organizations 
+                    </v-card-text>
+                </v-card>
+            </v-col>				
 				
+        </v-row>
+				
+	 
+	         <v-row                align="center"                justify="center"        >
+            <v-col>
+	
+	<h2>Technical details</h2>
+	
+	TODO
+	
+	</v-col>
+	</v-row>
+	 
 	 
     </v-container>
 </template>
