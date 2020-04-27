80fd50540fcaf36d872da2bdfe9f986e3b37c92a
Martin Breskvar <martin.breskvar@ijs.si>
2020-04-27 06:50:00 +0200

    Solution.vue initial

--- frontend/src/views/Solution.vue
+++ frontend/src/views/Solution.vue
@@ -7,11 +7,34 @@
                 justify="center"
         >
             <v-col>
+	
+	<h1>Connect all stakeholders and win</h1>
+	</v-col>
+        </v-row>
+	
+	<v-row class="mb-3">
+            <v-col class="col-12">
+
+<p>
+In an ideal world, each healthcare organization would have in-house software development engineers or do business with only one ISV. Unfortunately, 
+this is not the case. Healthcare IT ecosystems are incredibly complex, developed and maintained by many vendors and often poorly integrated. 
+This results in technical difficulties and data integrity issues on a daily basis. It is becoming increasingly difficult to obtain quality data. 
+
+</p>
+
+<p>
+MeDEP solves many issues related to data acquisition and comes in two flawors: MeDEP local and MeDEP federated. <br />
+
+MeDEP local is installed within the organization and is integrated with existing information systems by using specially crafted MeDEP connectors.
 
 
+</p>
 
             </v-col>
         </v-row>
+				
+				
+	 
     </v-container>
 </template>
 
