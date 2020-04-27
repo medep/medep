a6611a7c833f8fa38d5c421f3f1045a3d3e3cfbc
Janez Kranjc <janez.kranjc@ijs.si>
2020-04-27 03:14:56 +0200

    Fetch Data

--- frontend/src/views/DatasetDirectory.vue
+++ frontend/src/views/DatasetDirectory.vue
@@ -10,16 +10,46 @@
 
                 <h1>Dataset directory</h1>
 
-                <p>TODO</p>
-				
+                <v-data-table :items="data" :headers="headers" class="elevation-1" :loading="table_loading"
+                              loading-text="Loading... Please wait">
+                </v-data-table>
+
             </v-col>
         </v-row>
     </v-container>
 </template>
 
 <script>
-
     export default {
-        name: 'DatasetDirectory'
+        name: 'DatasetDirectory',
+        data: () => ({
+            table_loading: true,
+            data: [],
+            headers: [
+                {
+                    text: 'Name',
+                    sortable: true,
+                    value: 'datasetName',
+                },
+                {
+                    text: 'Authors',
+                    sortable: false,
+                    value: 'authors'
+                },
+                {
+                    text: 'Owners',
+                    sortable: false,
+                    value: 'owners',
+                }
+            ]
+        }),
+        mounted() {
+            this.table_loading = true;
+            fetch('https://umcm.medep.org/data/')
+                .then(res => res.json())
+                .then(res => {
+                    this.data = res
+                }).finally(() => (this.table_loading = false))
+        }
     }
 </script>
