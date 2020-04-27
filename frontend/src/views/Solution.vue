2557c534b7a0190dd2c26993b2a80f86d10074b9
Martin Breskvar <martin.breskvar@ijs.si>
2020-04-27 07:52:17 +0200

    Solution.vue

--- frontend/src/views/Solution.vue
+++ frontend/src/views/Solution.vue
@@ -19,8 +19,7 @@ This results in technical difficulties and data integrity issues on a daily basi
 
 <br/>
 
-MeDEP solves many issues related to data acquisition and comes in two flawors: MeDEP local and MeDEP federated. <br />
-
+MeDEP solves issues related to data acquisition and comes in two flawors: MeDEP local and MeDEP federated. Choose your weapon!<br />
 </p>
 
             </v-col>
@@ -33,16 +32,22 @@ MeDEP solves many issues related to data acquisition and comes in two flawors: M
             <v-col class="col-md-5">
 
                 <v-card>
+                    <v-card-subtitle><h3>MeDEP local - Join all organizational data sources</h3><br>
+                    </v-card-subtitle>
+
                     <v-img
                             class="white--text align-end"
-                            height="300px"
+                            width="400px"
                             src="../assets/medep-local.png"
                     ></v-img>
-                    <v-card-subtitle>MeDEP local: join all organizational data sources
-                    </v-card-subtitle>
                     <v-card-text>
+					
                         MeDEP local is installed within the organization and is integrated with existing information systems by using specially crafted MeDEP connectors.
-						The connectors are tailor-made by us to ensure 100% reliability and security.
+						The connectors are tailor-made by us to ensure 100% reliability and security.<br><br>
+						Your IT department stays in full control over data access and policies. We grant and revoke user access automatically!<br><br>
+						Data owners decide whether data can be accessed by MeDEP users.<br><br>
+						  
+						Upper management can observe KPIs in real time!
                     </v-card-text>
                 </v-card>
             </v-col>
@@ -50,16 +55,24 @@ MeDEP solves many issues related to data acquisition and comes in two flawors: M
    <v-col class="col-md-5">
 
                 <v-card>
+				<v-card-subtitle><h3>MeDEP federated - Harness the real power of connected world</h3><br>
+                    </v-card-subtitle>
                     <v-img
                             class="white--text align-end"
-                            height="300px"
-                            src="../assets/medep-local.png"
+                            width="400px"
+                            src="../assets/medep-federated.png"
                     ></v-img>
-                    <v-card-subtitle>MeDEP federated: Harness the real power of connected world
-                    </v-card-subtitle>
+                    
                     <v-card-text>
-                        MeDEP federated connects multiple MeDEP local instances. With this, different organizations can share research and current business data.
-						All involved organizations 
+                        MeDEP federated connects multiple MeDEP local instances. Now, different organizations can share research and current business data.
+						<br>
+						<br>Sharing data can:
+<ul>
+<li>improve patient treatment and research</li>
+<li>optimize purchase plans by monitoring what is in stock and linking that data to financial plans</li>
+<li>aid with resource planning in disaster scenarios or epidemic situations</li>
+</ul>
+Imagine the possibilities!
                     </v-card-text>
                 </v-card>
             </v-col>				
@@ -72,7 +85,7 @@ MeDEP solves many issues related to data acquisition and comes in two flawors: M
 	
 	<h2>Technical details</h2>
 	
-	TODO
+	Our solution
 	
 	</v-col>
 	</v-row>
